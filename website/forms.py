b2953bda9f79b88fadb4ad228441c2416bd2b7ce
dijair vieira <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2019-05-26 23:53:01 -0300

    uma_parte

--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,34 @@ from django import forms
 from django.core.mail import send_mail
 
 class ContatoForm(forms.Form):
+    ASSUNTOS =(
+        ('B', 'Bug'),
+        ('R', 'Reclamação'),
+        ('S', 'Sugestão'),
+    )   
+    RESPOSTAS = (
+        ('T', 'Telefone'),
+        ('E', 'E-mail'),
+    )
+    nome = forms.CharField(
+        min_length = 3,
+        max_length = 120
+    )
+    email = forms.EmailField(
+        required = False
+    )
+    telefone = forms.CharField(
+        required = False
+    )
+    assunto = forms.ChoiceField(
+        choices = ASSUNTOS
+    )
+    mensagem = forms.CharField()
+    resposta = forms.MutipleChoiceField(
+        required = False
+        choices = RESPOSTAS
+    )
+
 
     def enviar_email(self):
         pass
